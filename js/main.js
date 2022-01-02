1970b9579ed9d6365d5efdb4d5ac4475f7588219
GiuseppeM <fixercrade@hotmail.it>
2022-01-02 14:19:35 +0100

    fix

--- js/main.js
+++ js/main.js
@@ -1,52 +1,11 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 53ca73be3fe2e709932b56594ce9be6f1de92a9f
-window.onload = () => {
-
-    'use strict';
-
-
-
-    if ('serviceWorker' in navigator) {
-
-        navigator.serviceWorker
-
-            .register('./sw.js').then(function (registration) {
-
-                
-
-<<<<<<< HEAD
-            // Service worker registered correctly.
-=======
-            // Service worker registered correctly.​
->>>>>>> 53ca73be3fe2e709932b56594ce9be6f1de92a9f
-
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-
-        }, 
-<<<<<<< HEAD
-        
-=======
->>>>>>> 53ca73be3fe2e709932b56594ce9be6f1de92a9f
-        function (err) {
-
-            
-
-<<<<<<< HEAD
-        // Troubles in registering the service worker. :(
-=======
-        // Troubles in registering the service worker. :(​
->>>>>>> 53ca73be3fe2e709932b56594ce9be6f1de92a9f
-
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
         console.log('ServiceWorker registration failed: ', err);
-
+      });
     });
-
-}
-
-<<<<<<< HEAD
-};
-=======
-}
->>>>>>> 53ca73be3fe2e709932b56594ce9be6f1de92a9f
+  }
--- sw.js
+++ sw.js
@@ -1,49 +1,68 @@
-let cacheName = 'pwa01';
-
-let filesToCache = [
-
-    'index.html',
-
-<<<<<<< HEAD
-    'style.css',
-
-    'js/main.js'
-=======
-    'css/style.css',
-
-    'js/main.js'
-
->>>>>>> 53ca73be3fe2e709932b56594ce9be6f1de92a9f
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/css/style.css',
+  '/js/main.js'
 ];
 
-/* Start the service worker and cache all of the app's content */
-
-self.addEventListener('install', function(e) {
-
-    e.waitUntil(
-
-        caches.open(cacheName).then(function(cache) {
-
-            return cache.addAll(filesToCache);
-
-        })
-
-    );
-
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-/* Serve cached content when offline */
-
-self.addEventListener('fetch', function(e) {
-
-    e.respondWith(
-
-        caches.match(e.request).then(function(response) {
-
-            return response || fetch(e.request);
-
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+  
+          return fetch(event.request).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
         })
+      );
+  });
 
+  self.addEventListener('activate', function(event) {
+
+    var cacheAllowlist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+  
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheAllowlist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
     );
-
-});
+  });
